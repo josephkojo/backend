516df05a53217cc86bd767a7c6854364012f099a
Joseph Aidoo <kojoaidoi@gmail.com>
2024-06-03 19:22:04 +0000

    added email verification to the register controller

--- src/main/java/com/springDevelopers/Backend/Auth/AuthenticationController.java
+++ src/main/java/com/springDevelopers/Backend/Auth/AuthenticationController.java
@@ -1,8 +1,10 @@
 package com.springDevelopers.Backend.Auth;
 
+import com.springDevelopers.Backend.DTO.MailBody;
 import com.springDevelopers.Backend.Entities.User;
 import com.springDevelopers.Backend.Enums.Role;
 import com.springDevelopers.Backend.Repositories.UserRepository;
+import com.springDevelopers.Backend.Services.EmailService;
 import com.springDevelopers.Backend.Services.UserService;
 import com.springDevelopers.Backend.SpringSecurity.JwtService;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,8 @@ public class AuthenticationController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final EmailService emailService;
+
     private final UserRepository userRepository;
     private final JwtService jwtService;
     @PostMapping("/register")
@@ -35,6 +39,15 @@ public class AuthenticationController {
             }
 
         }
+        MailBody mailBody = new MailBody();
+        mailBody.setRecipient(registerRequest.getEmail());
+        mailBody.setText("Congratulations Dear "+ registerRequest.getFirstname() + " your have been successfully  registered to" +
+                "the Access key Manager Platform ");
+        mailBody.setSubject("Access Key Manager");
+        emailService.simpleMailSender(mailBody);
+
+
+
         AuthenticateResponse authenticateResponse = this.authenticationService.registerUser(registerRequest);
         return new ResponseEntity<>(authenticateResponse, HttpStatus.CREATED);
 
